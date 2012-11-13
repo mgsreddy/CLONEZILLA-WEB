5ff9fd0cfc42ab7d504892fef357ec438e8bf53f
skybot <robert.schuetze@gmail.com>
2012-11-13 18:04:47 +0100

    Change style in save image page page

--- save_image_page.php
+++ save_image_page.php
@@ -86,7 +86,7 @@ window.onload=function(){
    <li><a href='/setup.php'><span><?php echo $lang['MENU_SETUP']; ?></span></a></li>
       </ul>    </nav>
   </header>
-  <section id="content">
+<section id="content">
     <article>
       <h2><?php echo $lang['SAVE_H1']; ?></h2>
       <p>
@@ -94,55 +94,51 @@ window.onload=function(){
 
 
 <form id="saveform" class="rounded" method="post" action="jump_save.php">
-
- 
-<div class="field">
+<div id="safe_form_left"> 
+	<div class="field">
     	<label for="image_name"><?php echo $lang['SAVE_FORM_IMAGE_NAME']; ?></label>
 		<input type="text" class="input" name="image_name" id="image_name" title= <?php echo $lang['SAVE_FORM_IMAGE_NAME']; ?> >
+	</div>
 
-<div id="input_long">
+	<div class="styled-select">
+		<label for="part"><?php echo $lang['SAVE_FORM_HARDDRIVE']; ?></label> <!-- Abfrage nach Festplatte sda/sdb -->
+			<select class="select" name="part" id="part">
+				<option value="sda"><?php echo $lang['SAVE_FORM_HARDDRIVE_1']; ?></option>
+				<option value="sdb"><?php echo $lang['SAVE_FORM_HARDDRIVE_2']; ?></option>
+			</select>
+	</div>
+	<div class="styled-select">
+		<label for="os"><?php echo $lang['SAVE_FORM_OS']; ?></label> <!-- Abfrage nach Betriebssystem -->
+			<select class="select" name="os" id="os">
+				<option value="WINDOWS_XP">Windows XP</option>
+				<option value="WINDOWS_7">Windows 7</option>
+				<option value="LINUX">Linux</option>
+			</select>
+	</div>
+	<div class="field">
+    	<label for="hw_type"><?php echo $lang['SAVE_FORM_HARDWARE']; ?></label> <!-- Abfrage nach Hardware -->
+		<input type="text" class="input" name="hw_type" id="hw_type" title="Bitte den Rechner Typ angeben z.B Celsius 230..." />
+						
+	</div>
+
+		<input type="button"  class="button" value="<?php echo $lang['SAVE_FORM_BUTTON_START']; ?>" onclick="myFrm = document.getElementById('saveform'); myFrm.action='jump_save.php?what=start'; myFrm.submit();" />
+		<input type="button"  class="button" value="<?php echo $lang['SAVE_FORM_BUTTON_STOP']; ?>" onclick="myFrm = document.getElementById('saveform');myFrm.action='jump_save.php?what=stop'; myFrm.submit();" />
+</div>
+<div id="safe_form_right"> 
 <label for="image_description"><?php echo $lang['SAVE_FORM_IMAGE_DESCRIPTION']; ?></label>
 <textarea id="image_description" name="image_description" cols="55" rows="13"></textarea>
-		
-</div>				
-
-</div>
-
-
- 
-<div class="styled-select">
-	<label for="part"><?php echo $lang['SAVE_FORM_HARDDRIVE']; ?></label> <!-- Abfrage nach Festplatte sda/sdb -->
-						<select class="select" name="part" id="part">
-						<option value="sda"><?php echo $lang['SAVE_FORM_HARDDRIVE_1']; ?></option>
-						<option value="sdb"><?php echo $lang['SAVE_FORM_HARDDRIVE_2']; ?></option>
-</select>
-</div>
-<div class="styled-select">
-	<label for="os"><?php echo $lang['SAVE_FORM_OS']; ?></label> <!-- Abfrage nach Betriebssystem -->
-						<select class="select" name="os" id="os">
-						<option value="WINDOWS_XP">Windows XP</option>
-						<option value="WINDOWS_7">Windows 7</option>
-						<option value="LINUX">Linux</option>
-</select>
-</div>
-<div class="field">
-    <label for="hw_type"><?php echo $lang['SAVE_FORM_HARDWARE']; ?></label> <!-- Abfrage nach Hardware -->
-						<input type="text" class="input" name="hw_type" id="hw_type" title="Bitte den Rechner Typ angeben z.B Celsius 230..." />
-						
-</div>
-
-
-
-
-<input type="button"  class="button" value="<?php echo $lang['SAVE_FORM_BUTTON_START']; ?>" onclick="myFrm = document.getElementById('saveform'); myFrm.action='jump_save.php?what=start'; myFrm.submit();" />
-<input type="button"  class="button" value="<?php echo $lang['SAVE_FORM_BUTTON_STOP']; ?>" onclick="myFrm = document.getElementById('saveform');myFrm.action='jump_save.php?what=stop'; myFrm.submit();" />
-
-
-
-
+</div>	
 
 
 </form>
+
+
+
+
+
+
+
+
 </p>
     </article>
   </section>
@@ -162,3 +158,13 @@ window.onload=function(){
     <a href="http://www.sinupedia.de">by skybot</a>
   </footer>
 </div>
+
+
+
+
+
+<div id="input_long">
+<label for="image_description"><?php echo $lang['SAVE_FORM_IMAGE_DESCRIPTION']; ?></label>
+<textarea id="image_description" name="image_description" cols="55" rows="13"></textarea>
+		
+</div>				
